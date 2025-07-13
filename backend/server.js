58a5cf65880cd94523e5d444bdec9f819b40f1ec
Arunit <arunitchakraborty2006@gmail.com>
2025-07-13 12:38:58 +0530

    Implement backend structure with database connection and error handling

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const connectDb = require("./db/dbConnect.js");
+const app = require("./app.js");
+const mongoose = require("mongoose");
+require("dotenv").config({
+  path: "./.env",
+});
+
+let server;
+
+//connect databse and then start the server
+(async () => {
+  try {
+    await connectDb();
+    const port = process.env.PORT || 8000;
+    server = app.listen(port, () => {
+      console.log(`server listening on port: ${port}`);
+    });
+  } catch (error) {
+    console.log("Connection failed", error);
+    process.exit(1);
+  }
+})();
+
+//gracefully close server along with the database
+process.on("SIGINT", async () => {
+  console.log("\nrecieved SIGINT, shutting down.....");
+  if (server) await server.close(() => console.log("server shut down"));
+  await mongoose.connection.close();
+  process.exit(0);
+});
