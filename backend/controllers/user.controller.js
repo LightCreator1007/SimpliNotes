cafd8cea6201a65b8aecc75824ff593349fcb84f
Arunit <arunitchakraborty2006@gmail.com>
2025-08-06 18:29:53 +0530

    tweaks

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -229,4 +229,13 @@ const getUser = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, req.user, ""));
 });
 
-export { registerUser, login, logout, renewSession, changeAvatar };
+export {
+  registerUser,
+  login,
+  logout,
+  renewSession,
+  changeAvatar,
+  getUser,
+  changePassword,
+  updateUser,
+};
